2bb0ab4262c4a7677b733ba92ee33fa4d376c585
haki9975 <elias.robert.hakim@gmail.com>
2021-02-26 17:46:55 -0500

    Done! In a simplistic, unstylized way.

--- README.md
+++ README.md
@@ -1,42 +1,23 @@
-# FoodFinder
+Welcome! This program is designed to help you select a recipe from a list of international dishes. This will be helpful for when programming depletes your appetite and you approach the hangry danger zone. No one wants to be hungry without inspiration for what to eat...
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/food_finder`. To experiment with that code, run `bin/console` for an interactive prompt.
 
-TODO: Delete this and the text above, and describe your gem
-
-## Installation
-
-Add this line to your application's Gemfile:
-
-```ruby
-gem 'food_finder'
-```
-
-And then execute:
-
-    $ bundle install
-
-Or install it yourself as:
-
-    $ gem install food_finder
 
 ## Usage
 
-TODO: Write usage instructions here
+Navigate to the correct root directory for Food finder. Once in the directory, you can run the program by typing bin/run.
+Once in the program, follow the prompts on the CLI, and pick a meal by name. Each meal will contain a link to a recipe and a link to a youtube video. If either is absent, they will remain blank, but rest assured you will be able to find your recipe by following the other link. 
 
-## Development
+Happy eating, and don't get your greasy fingers on the keyboard! 
 
-After checking out the repo, run `bin/setup` to install dependencies. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
 
-To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and the created tag, and push the `.gem` file to [rubygems.org](https://rubygems.org).
 
 ## Contributing
 
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/food_finder. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [code of conduct](https://github.com/[USERNAME]/food_finder/blob/master/CODE_OF_CONDUCT.md).
+Bug reports and pull requests are welcome on GitHub at https://github.com/haki9975/food_finder. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [code of conduct](https://github.com/[USERNAME]/food_finder/blob/master/CODE_OF_CONDUCT.md).
 
 ## License
 
-The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
+The app is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
 
 ## Code of Conduct
 
--- lib/food_finder/cli.rb
+++ lib/food_finder/cli.rb
@@ -1,55 +1,93 @@
 require 'pry'
 #CLI Controller, responsible for welcoming user and managing user input
 class FoodFinder::CLI
-  @@all = []
-
+ 
   def call
     FoodFinder::API.recipes
     introduction
   end
    
   def introduction
-          input = nil
-    until input == "N" do
-       puts "Are you hungry?  Enter Y to continue or N to quit"
-       input= gets.chomp.downcase
-      if input== "y"  
-          display_recipes
-       elsif input == "n"
-          goodbye_1
-       else
-          puts "I didn't understand that, please enter Y or N"
-      end
+    puts "Are you hungry?  Enter Y to continue or N to quit"
+    input = gets.chomp
+
+    if input == "y"
+      display_recipes
+    elsif input == 'n'
+      goodbye_1
+    else
+      puts "I didn't understand that, please try again"
+      introduction
+    end
+  end
+  #         input = nil
+  #         puts "starts a loop A"
+  #  # until input == "N" do
+  #      puts "Are you hungry?  Enter Y to continue or N to quit"
+  #      input= gets.chomp.downcase
+  #     if input== "y"  
+  #         display_recipes
+  #      elsif input == "n"
+  #         goodbye_1
+  #      else
+  #         puts "I didn't understand that, please enter Y or N"
+    #   end
          
           
-    end
-  end
+    # end
+    # puts "ends a loop A"
+  #end
 
   def display_recipes
-          input = nil
-          puts recipe_displayall
+          # input = 0
+        puts recipe_displayall
         puts "Would you like to cook any of these recipes? Scroll up to see recipies and select recipe by entering the number. Type 27 to quit."
        
-       # input = gets.chomp
-    until input == 27 do
-      input = gets.chomp.to_i
-        if input < 26 && input > 0
-          input -=1
-         puts "#{recipe_all(input).strSource} Here is your #{recipe_all(input).strMeal} recipe link and here is a link to video instruction: #{recipe_all(input).strYoutube}."
-        
-         puts "Press 27 if you are ready to exit. Press 26 if you would like to see the recipe list again."
-         input = gets.chomp.to_i
-        elsif input == 26
-          display_recipes
-        elsif input == 27
+        input = gets.chomp
+      if input.to_i == 27
           goodbye_2
-        elsif input > 27 || input.class != Integer
-          puts "I didn't understand that, please try again"
-        end
-          input = gets.chomp
-    end
+      else
 
+        recipe = recipe_all(input.to_i - 1)
+
+        puts "Here is your #{recipe.strMeal} recipe link: #{recipe.strSource}."
+        puts "Here is a link to video instruction for your #{recipe.strMeal}:  #{recipe.strYoutube}."
+        puts "Press 27 if you are ready to exit. Press 26 if you would like to see the recipe list again."
+
+        input = gets.chomp
+        if input.to_i == 27
+          goodbye_2
+        elsif input.to_i == 26
+          display_recipes
+        else
+          puts "I didn't understand that, please try again"
+          display_recipes
+        end
+      end
+      # puts "start loop B"
+    # until input == 27 do
+    #   input = gets.chomp.to_i
+      
+    #     if input < 26 && input > 0
+    #       input -=1
+    #      puts "Here is your #{recipe_all(input).strMeal} recipe link:#{recipe_all(input).strSource}"
+    #      puts "Here is a link to video instruction for your #{recipe_all(input).strMeal}:  #{recipe_all(input).strYoutube}."
+    #      puts "Press 27 if you are ready to exit. Press 26 if you would like to see the recipe list again."
+    #      input = gets.chomp.to_i
+    #     elsif input == 26
+    #       display_recipes
+    #     elsif input == 27
+    #       goodbye_2
+    #     elsif input.class != Integer || input > 27 || input <= 0
+    #       puts "I didn't understand that, please try again"
+    #     end
+    #       input = gets.chomp
+    #  end
+        
+    #     puts "end loop B"
   end
+
+  
 
   def recipe_accessor
     FoodFinder::Recipe.some.collect.with_index(1) {|meal, index| "#{index}. #{meal}"}  
